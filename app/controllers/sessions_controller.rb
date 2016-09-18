15e02e61283455ebd24dd4fe07713860d99090bf
Ora-Kool <etingemabian@gmail.com>
2016-09-19 00:37:30 +0100

    session added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
+
+  end
+  def create
+  	user = HoacUser.find_by(email: params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+  		# log in the user
+  		log_in user
+  		redirect_to user
+  	else
+  		#render the login page again for the user to to try again
+  		flash.now[:danger] = 'Invalid email/password combination!'
+  	render 'new'
+  end
+  end
+
+  def destroy
+  	
   end
 end
